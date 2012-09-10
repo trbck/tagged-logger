72626778a6954f91b1b58b60733ec6d7ffbed55c
Roman Imankulov <roman.imankulov@gmail.com>
2012-09-11 01:00:28 +0600

    Version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author_email='roman.imankulov@gmail.com',
     url='http://github.com/imankulov/tagged-logger',
     packages=['tagged_logger', ],
-    version='0.2',
+    version='0.3',
     license='BSD',
     install_requires=[
         'redis',
